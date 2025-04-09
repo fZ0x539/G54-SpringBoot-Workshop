92607d41f85407359321065077d991c1436cd540
Daniel <daka98001@gmail.com>
2025-04-09 07:35:19 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g54springbootworkshop/G54SpringBootWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g54springbootworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G54SpringBootWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G54SpringBootWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g54springbootworkshop/G54SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g54springbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G54SpringBootWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
